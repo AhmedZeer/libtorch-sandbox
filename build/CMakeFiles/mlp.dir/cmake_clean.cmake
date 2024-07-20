b22927205690ba55fb44f72ff1d0debc69e33f6b
ahmedzeer <ahmed.zeer@std.yildiz.edu.tr>
2024-07-20 09:50:02 +0300

    MLP Fix

---
+++ build/CMakeFiles/mlp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mlp.dir/mlp.cpp.o"
+  "CMakeFiles/mlp.dir/mlp.cpp.o.d"
+  "mlp"
+  "mlp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mlp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
